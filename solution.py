d3621b027c2e4690db2c446eb9cc921c085e5677
mp11006 <98921624+mp11006@users.noreply.github.com>
2022-02-02 18:26:07 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,19 +9,19 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
-    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
+    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "mtls"
-    elif question == "Are encoding and encryption the same? - Yes/No"
+    elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
-    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
-    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code"
+    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
         answer = "148117d9b6892cd860f08ef182a5de65"
-    elif question == "Is MD5 a secured hashing algorithm? - Yes/No"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
-    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         asnwer = 4
-    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 1
     return(answer)
 # Complete all the questions.
